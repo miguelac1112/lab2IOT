00758d395c6bbcf2b045b165008ed2262d2f04ad
Miguel <a20190050@pucp.edu.pe>
2023-09-16 15:42:18 -0500

    hay internet

--- app/src/main/java/com/example/lab2iot/MainActivity.java
+++ app/src/main/java/com/example/lab2iot/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.lab2iot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +17,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Tiene internet: " + tengoInternet(), Toast.LENGTH_SHORT).show();
     }
 
     public void inicio(View view){
         Intent intent = new Intent(this, signUp.class);
         startActivity(intent);
+    }
+
+    public boolean tengoInternet() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetworkInfo = manager.getActiveNetworkInfo();
+        boolean tieneInternet = activeNetworkInfo != null && activeNetworkInfo.isConnected();
+
+        Log.d("msg-test", "Internet: " + tieneInternet);
+
+        return tieneInternet;
     }
 }
