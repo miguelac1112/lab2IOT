33cc31062d0209e488bfb3137fcad267e63a4b76
Miguel <a20190050@pucp.edu.pe>
2023-09-16 02:07:17 -0500

    primer avance

---
+++ app/src/main/java/com/example/lab2iot/cronometroUsuario.java
@@ -0,0 +1,14 @@
+package com.example.lab2iot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cronometroUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cronometro_usuario);
+    }
+}
---
+++ app/src/main/java/com/example/lab2iot/inicioUsuario.java
@@ -0,0 +1,14 @@
+package com.example.lab2iot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class inicioUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_usuario);
+    }
+}
