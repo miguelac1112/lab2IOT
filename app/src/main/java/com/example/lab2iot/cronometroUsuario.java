97a3b576090993b7f4210f8cb436e52610c956af
Miguel <a20190050@pucp.edu.pe>
2023-09-17 09:54:51 -0500

    iknow mami iknow

--- app/src/main/java/com/example/lab2iot/cronometroUsuario.java
+++ app/src/main/java/com/example/lab2iot/cronometroUsuario.java
@@ -3,12 +3,90 @@ package com.example.lab2iot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Chronometer;
+
 
 public class cronometroUsuario extends AppCompatActivity {
+
+
+    private Chronometer chronometer;
+    private long pauseOffset;
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cronometro_usuario);
+
+        chronometer = findViewById(R.id.chronometer);
+        chronometer.setFormat("%s");
+
+        // Configura los listeners de los botones
+        findViewById(R.id.button5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iniciarCronometro(v);
+            }
+        });
+
+        findViewById(R.id.button8).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                detenerCronometro(v);
+            }
+        });
+
+        findViewById(R.id.button10).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                detenerCronometro(v);
+            }
+        });
+
+        findViewById(R.id.button9).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reanudarCronometro(v);
+            }
+        });
+
+        findViewById(R.id.button11).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                limpiarCronometro(v);
+            }
+        });
+    }
+
+    public void iniciarCronometro(View view) {
+        if (!running) {
+            chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+            chronometer.start();
+            running = true;
+        }
+    }
+
+    public void detenerCronometro(View view) {
+        if (running) {
+            chronometer.stop();
+            pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
+            running = false;
+        }
+    }
+
+    public void reanudarCronometro(View view) {
+        if (!running) {
+            chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
+            chronometer.start();
+            running = true;
+        }
+    }
+
+    public void limpiarCronometro(View view) {
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        pauseOffset = 0;
+        running = false;
     }
 }
